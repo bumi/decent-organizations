1379d5a76fc442305c6bbcdd343fd866898fa4ff
Plsr <christianpoplawski@gmail.com>
2018-02-13 18:52:59 +0100

    Validations for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :posts
+
+  validates :name, presence: true, length: { maximum: 255 }
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:category) { FactoryBot.build(:category) }
+
+  it 'is valid with valid attributes' do
+    expect(category).to be_valid
+  end
+
+  it 'is saved with valid attributes' do
+    expect(category.save).to be true
+  end
+
+  it 'is not valid without a name' do
+    category.name = nil
+    expect(category).not_to be_valid
+  end
+
+  it 'is not valid with a too long name' do
+    category.name = "a" * 256
+    expect(category).not_to be_valid
+  end
 end
