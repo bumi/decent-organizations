8e238867482f735d1794b6a89d0d9b5c1d7beb43
Plsr <christianpoplawski@gmail.com>
2018-02-22 10:56:29 +0100

    Add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
     end
   end
   resources :categories, only: [:show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # Static Pages
+  get 'pages/about'
+  get 'pages/idea'
 end
