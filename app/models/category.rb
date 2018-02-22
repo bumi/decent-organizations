7ee3cdf0155feded9278b6c33f8a4df03434479f
Plsr <christianpoplawski@gmail.com>
2018-02-22 14:29:41 +0100

    Use category name as slug

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
+  extend FriendlyId
+  friendly_id :name
+
   has_and_belongs_to_many :posts
 
   validates :name, presence: true, length: { maximum: 255 }, uniqueness: true
