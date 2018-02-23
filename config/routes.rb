63aa06bba827b90e8c8583979230e4c3a4824c5d
Plsr <christianpoplawski@gmail.com>
2018-02-23 10:20:10 +0100

    Move pages and categories to root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   root "posts#index"
+
+  # Static Pages
+  get 'about', to: 'pages#about'
+  get 'idea', to: 'pages#idea'
+
   resources :posts do
     resources :comments, only: [:create]
     member do
       put :upvote
     end
   end
-  resources :categories, only: [:show]
+  resources :categories, only: [:show], path: '/'
   
-  # Static Pages
-  get 'pages/about'
-  get 'pages/idea'
+
 end
