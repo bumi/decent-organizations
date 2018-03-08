b89cf025f64c2594c4ebedd5197b2c8f01d61ce0
Plsr <christianpoplawski@gmail.com>
2018-03-08 18:05:48 +0100

    First steps in posts styling

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   def show
     @category = Category.friendly.find(params[:id])
+    @posts = @category.posts.paginate(page: params[:page], per_page: 10)
   end
 end
