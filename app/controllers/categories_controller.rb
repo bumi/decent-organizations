03b305d5faa6ebd3d6f9cd9379e4f33b4c4d65f5
Christian Poplawski <christianpoplawski@gmail.com>
2018-10-09 14:43:37 +0200

    Add performed? test to categories#show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,6 +4,7 @@ class CategoriesController < ApplicationController
 
   def show
     ensure_path_or_redirect_to(category)
+    return if performed?
     @posts = category.posts.includes(:comments, :categories).paginate(page: params[:page], per_page: 30)
   end
 
