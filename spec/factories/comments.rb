8bf2860d94c69ffcef4eb2c46cf7d5f98c6dfbb5
Michael Bumann <hello@michaelbumann.com>
2019-07-10 11:54:34 +0200

    Update factories syntax

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :comment do
-    text "MyText"
-    name "Commenter name"
+    text { "MyText" }
+    name { "Commenter name" }
     association :post, factory: :post_with_categories
   end
 end
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,17 +1,17 @@
 FactoryBot.define do
   factory :post do
-    title "My Title"
-    description "Description of the post that is long enough to be valid."
-    url "https://example.com"
+    title { "My Title" }
+    description { "Description of the post that is long enough to be valid." }
+    url { "https://example.com" }
 
     factory :post_with_categories do
       transient do
-        categories_count 2
+        categories_count { 2 }
       end
 
       # Add categories to post
       after(:build) do |post, evaluator|
-        
+
         post.categories << create_list(:category, evaluator.categories_count)
       end
     end
